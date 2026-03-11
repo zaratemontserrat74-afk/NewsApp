dbb424419a1f76446d2f15b06840f5ff205a8998
Montse <zaratemontserrat74@gmail.com>
2026-03-11 15:39:45 -0600

    Imagenes

--- app/src/main/java/com/pajasoft/newsapp/models/News.kt
+++ app/src/main/java/com/pajasoft/newsapp/models/News.kt
@@ -10,21 +10,21 @@ val newsList = listOf(
     Noticia(
         title = "El presidente de EE.UU. no muestra sifnos de arrepentimiento.",
         fecha = "febrero 08 - 2024",
-        img = "https://ichef.bbci.co.uk/news/1024/branded_news/c5d7/live/9f0195a0-d517-11ef-94cb-5f844ceb9e30.jpg"
+        img = "https://estaticos.elcolombiano.com/binrepository/849x565/69c0/780d565/none/11101/VLMA/donald-trump-salario-presidente-esta_46472239_20241107153827.jpg"
     ),
     Noticia(
         title = "Bañarse en la pscina del desierto de Cleopatra",
         fecha = "febrero 11 - 2024",
-        img = "https://s1.elespanol.com/2018/09/21/cultura/historia/curiosidades_de_la_historia-historia-historia_339728816_98134230_1706x1280.jpg"
+        img = "https://thumbs.dreamstime.com/z/reina-cleopatra-en-el-papiro-4052746.jpg"
     ),
     Noticia(
         title = "Gigantes tecnológicos",
         fecha = "febrero 12 - 24",
-        img  = "https://www.gaceta.unam.mx/wp-content/uploads/2023/04/230424-aca4-des-f1-gigantes-tecnologicos-internet.jpg"
+        img  = "https://elceo.com/wp-content/uploads/2021/04/empresas.jpg"
     ),
     Noticia(
         title = "El rover de Marte envia nuevas imágenes",
         fecha = "febrero 14 - 2024",
-        img = "https://unamglobal.unam.mx/wp-content/uploads/2018/02/91FA91A5-4FEF-44E7-9AAF-5AC51E577B0C.jpeg"
+        img = "https://www.univision.com/_next/image?url=https%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2Fassets%2Fvixes%2Fr%2Frover_marte_cielo_azul_3.jpg&w=1280&q=75"
     )
 )
